f9cc3ddc2f50b3d8ff47d9884fb7d51fde925b36
Taylorbelk1 <belktaylor12@gmail.com>
2019-05-11 14:39:20 -0500

    added basic routes so we can navigate to different components to see what we are working on

---
+++ pizza-time/src/friends-lists/friends-list.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FriendsList = () => {
+    return(
+        <div>
+            FriendsList Works!
+        </div>
+    );
+}
+
+export default FriendsList
---
+++ pizza-time/src/landing-page/landing.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Landing = () => {
+    return(
+        <div>
+            Landing Works!
+        </div>
+    );
+}
+
+export default Landing
