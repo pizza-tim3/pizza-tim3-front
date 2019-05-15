89455427ee9c53e3985ffd30379957fc1b0d43af
unknown <noah.stoeckel@gmail.com>
2019-05-14 20:41:53 -0400

    Adds Auth observer effect,a private route,and a register route

--- pizza-time/src/App.js
+++ pizza-time/src/App.js
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import firebaseApp from "./firebase/firebaseApp";
 import "./App.css";
 import { Route } from "react-router-dom";
+import PrivateRoute from "./components/private-route/PrivateRoute";
+import Private from "./components/private/Private";
 import UserDashboard from "./components/user-dashboard/user-dashboard";
 import Nav from "./components/nav/nav";
 import Events from "./components/events/events";
@@ -13,6 +16,30 @@ import Register from "./components/register/Register";
 import PlacesSearch from "./components/events/search/places-search";
 
 function App() {
+  //placeholder state, realistically this will be in redux or some reducer
+  const [authenticated, setAuthenticated] = useState(false);
+
+  //check for auth only happens on mount
+  useEffect(() => {
+    //`onAuthStateChanged` is an `observer` which watches and runs a callback
+    //anytime a user's logged in status has been changed.
+    //this logic should be global.
+    firebaseApp.auth().onAuthStateChanged(user => {
+      console.log("AuthStateChanged");
+      //firebase
+      if (user) {
+        //HANDLE USER STATE IN REDUX/COMPONENT STATE
+        setAuthenticated(true);
+      } else {
+        //NO USER, CLEAR THE USER
+        setAuthenticated(false);
+      }
+    });
+  }, []); //<-The second argument is a useEffect's `deps` or `dependancies
+  //The effect will re-run ANY time ANY of the deps have changed
+  //Since there are NO deps this will only run on the mount
+  //making it very close to `componentDidMount`
+
   return (
     <div className="App">
       {/* setting these up seperately initially so we can nav to each path 
@@ -26,6 +53,13 @@ function App() {
       <Route path="/events/search" component={PlacesSearch} />
       <Route path="/favorites" component={Favorites} />
       <Route path="/friendslist" component={FriendsList} />
+
+      {/* <PrivateRoute
+        exact
+        path="/private"
+        component={Private}
+        authenticated={authenticated} //pass global authenticated status here
+      /> */}
       <Route path="/" component={Footer} />
     </div>
   );
