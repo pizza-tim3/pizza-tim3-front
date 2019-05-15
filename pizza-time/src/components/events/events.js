26141bb5bed13eb31094c81f553b369fd3ea40e0
120356aa <yurlovandrew@gmail.com>
2019-05-14 20:39:34 -0700

    refractored nav and footer

--- pizza-time/src/components/events/events.js
+++ pizza-time/src/components/events/events.js
@@ -1,9 +1,16 @@
 import React from 'react';
 
+import Nav from '../nav/nav.js';
+import Footer from '../footer/footer.js';
+
 const Events = () => {
     return(
-        <h2> Events Works! </h2>
-    )
-}
+        <div>
+            <Nav />
+            <h2> Events Works! </h2>
+            <Footer />
+        </div>
+    );
+};
 
 export default Events 
