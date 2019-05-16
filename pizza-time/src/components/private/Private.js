73614d2bda4da2ec036a5cc8fe1e62bb88a295d3
unknown <noah.stoeckel@gmail.com>
2019-05-15 23:11:39 -0400

    Updates private component
    Shows example on how to make a request to a restricted
    rout on the backend

--- pizza-time/src/components/private/Private.js
+++ pizza-time/src/components/private/Private.js
@@ -1,9 +1,32 @@
 import React from "react";
+import firebaseApp from "../../firebase/firebaseApp";
 
 export default function Private() {
+  const makeRequestWithFireBaseToken = async () => {
+    //get the token off of the current user
+    //idealistically this could be on some global state
+    const token = await firebaseApp.auth().currentUser.getIdToken();
+
+    const options = {
+      method: "GET", // *GET, POST, PUT, DELETE, etc.
+      headers: {
+        Authorization: token
+      }
+    };
+    const responseFromServer = await fetch(
+      "http://localhost:5000/restricted",
+      options
+    );
+    const json = await responseFromServer.json();
+    console.log(json);
+    // setData(responseFromServer);
+  };
   return (
     <div>
       <h1>This is a Private component</h1>
+      <button onClick={makeRequestWithFireBaseToken}>
+        Make Private Request
+      </button>
     </div>
   );
 }
