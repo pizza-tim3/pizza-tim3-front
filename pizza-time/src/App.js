f05dd5b27339070633f372df38b384accffca508
Alex Bezruchko <alex.bezruchko@gmail.com>
2019-05-16 20:16:09 -0400

    add admin folder, add usersList component and fetch users from backend

--- pizza-time/src/App.js
+++ pizza-time/src/App.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import UsersList from "./../src/admin/usersList";
 
 function App() {
   return (
     <div className="App">
       <h1>Pizza Time</h1>
+      <UsersList />
     </div>
   );
 }
