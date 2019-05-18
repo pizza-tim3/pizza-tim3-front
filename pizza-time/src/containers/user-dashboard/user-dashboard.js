5d4d029f08557a4772439d2c2daddb0a9d904d1e
120356aa <yurlovandrew@gmail.com>
2019-05-17 17:34:31 -0700

    userhome tabs now working

--- pizza-time/src/containers/user-dashboard/user-dashboard.js
+++ pizza-time/src/containers/user-dashboard/user-dashboard.js
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 
 import Nav from '../../components/home-header/home-header.js';
 import Footer from '../../components/footer/footer.js';
 
+import {
+    Wrap,
+    Inner,
+} from '../../styles/userhomeStyles.js';
+
 const UserDashboard = () => {
     return(
-        <div>
+        <>
             <Nav />
+            <Wrap>
+                <Inner>
+                    <Tabs defaultIndex={1} onSelect={index => console.log(index)}>
+                        <TabList className="tabBox">
+                            <Tab className="filterBtn" selectedClassName="filterBtnActive">Upcoming</Tab>
+                            <Tab className="filterBtn" selectedClassName="filterBtnActive">Pending</Tab>
+                            <Tab className="filterBtn" selectedClassName="filterBtnActive">Past</Tab>
+                        </TabList>
+                        <TabPanel className="tab">Tab 1</TabPanel>
+                        <TabPanel>Tab 2</TabPanel>
+                        <TabPanel>Tab 3</TabPanel>
+                    </Tabs>
+                </Inner>
+            </Wrap>
             {/* <Footer /> */}
-        </div>
+        </>
     );
 }
 
