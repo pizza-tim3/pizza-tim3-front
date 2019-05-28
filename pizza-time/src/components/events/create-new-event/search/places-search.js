3f5eec9801dd3af5944380b7a84e3bef5d492ec2
Taylorbelk1 <belktaylor12@gmail.com>
2019-05-27 21:00:19 -0500

    set places search up so that it will send the placeId to the state of the parent component

--- pizza-time/src/components/events/create-new-event/search/places-search.js
+++ pizza-time/src/components/events/create-new-event/search/places-search.js
@@ -1,17 +1,22 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import SearchBar from './search/search-bar';
 import GoogleMap from './map/map';
 
 const PlacesSearch = (props) => {
+
+    const [placeId, setPlaceId] = useState('');
+
+    const handleGetPlaceId = (id) => {
+        setPlaceId(id);
+    }
+
+    console.log(placeId)
     return(
         <div className="places-search-wrapper">
-            {/* <Nav /> */}
-                <h1>Pick a place near you or search somewhere else!</h1>
-                <SearchBar />
-                <GoogleMap />
-                <button onClick={() => {props.handleClick()}}>Next Step</button>
-                
-            {/* <Footer /> */}
+            <h1>Pick a place near you or search somewhere else!</h1>
+            <SearchBar />
+            <GoogleMap getId={handleGetPlaceId}/>
+            <button onClick={() => {props.handleClick()}}>Next Step</button>
         </div>
     )
 }
