3ae443d91e51aaf75e397f6463b6b824bf6184c3
unknown <noah.stoeckel@gmail.com>
2019-05-28 23:59:05 -0400

    makes an example of how to request the user specific request

--- pizza-time/src/components/private/Private.js
+++ pizza-time/src/components/private/Private.js
@@ -1,31 +1,56 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import firebaseApp from "../../firebase/firebaseApp";
 
-export default function Private() {
-  const makeRequestWithFireBaseToken = async () => {
-    //get the token off of the current user
-    //idealistically this could be on some global state
-    const token = await firebaseApp.auth().currentUser.getIdToken();
+const makeRequestWithFireBaseToken = async (url, method) => {
+  //get the token off of the current user
+  //idealistically this could be on some global state
+  const token = await firebaseApp.auth().currentUser.getIdToken();
 
-    const options = {
-      method: "GET", // *GET, POST, PUT, DELETE, etc.
-      headers: {
-        Authorization: token
-      }
-    };
-    const responseFromServer = await fetch(
-      "http://localhost:5000/restricted",
-      options
-    );
-    const json = await responseFromServer.json();
-    console.log(json);
-    // setData(responseFromServer);
+  const options = {
+    method: method.toUpperCase(), // *GET, POST, PUT, DELETE, etc.
+    headers: {
+      Authorization: token
+    }
   };
+  const responseFromServer = await fetch(url, options);
+  const json = await responseFromServer.json();
+  console.log(json);
+};
+
+export default function Private() {
+  const [uid, setUid] = useState(null);
+
+  // the current user unique ID
+  useEffect(() => {
+    const setID = async () => {
+      const uniqueId = await firebaseApp.auth().currentUser.uid;
+      setUid(uniqueId);
+    };
+    setID();
+  }, []);
+
   return (
     <div>
       <h1>This is a Private component</h1>
-      <button onClick={makeRequestWithFireBaseToken}>
+      <button
+        onClick={() => {
+          makeRequestWithFireBaseToken(
+            "http://localhost:5500/restricted",
+            "get"
+          );
+        }}
+      >
         Make Private Request
+      </button>
+      <button
+        onClick={() => {
+          makeRequestWithFireBaseToken(
+            `http://localhost:5500/restricted/${uid}`,
+            "get"
+          );
+        }}
+      >
+        Make Private User Specific Request
       </button>
       <button onClick={() => firebaseApp.auth().signOut()}>Logout</button>
     </div>
