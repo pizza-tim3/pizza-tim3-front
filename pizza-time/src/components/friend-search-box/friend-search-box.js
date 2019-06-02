22415ccbd7abd2f33f8b622039315875db80a816
unknown <noah.stoeckel@gmail.com>
2019-06-02 16:10:08 -0400

    Add friend search skeleton comp

---
+++ pizza-time/src/components/friend-search-box/friend-search-box.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function FriendSearchBox() {
+  return (
+    <>
+      <form>
+        <input type="text" name="fname" placeholder="Find a new friend" />
+      </form>
+    </>
+  );
+}
