06f27d40509e17c7a93a03b1e51b7fd50054d683
Taylorbelk1 <belktaylor12@gmail.com>
2019-06-04 19:55:48 -0500

    resolved a merge conflict

--- pizza-time/src/components/events/create-new-event/friendPicker/friendPicker.js
+++ pizza-time/src/components/events/create-new-event/friendPicker/friendPicker.js
@@ -1,7 +1,6 @@
 import React,  {useEffect, useState} from 'react';
 import axios from 'axios';
 import UserImage from '../../../../assets/user.png';
-import '../events.css';
 
 import { FriendPickerWrap, PlacesHeading, Button } from '../../../../styles/friendPickerStyles';
 
