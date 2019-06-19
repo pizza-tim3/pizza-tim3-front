dac5835d907cecb2e74f794ddf5b686748210f85
unknown <noah.stoeckel@gmail.com>
2019-06-18 20:11:04 -0400

    Finish authorized request function

--- pizza-time/src/firebase/authorizedRequest.js
+++ pizza-time/src/firebase/authorizedRequest.js
@@ -3,17 +3,23 @@ import firebaseApp from "./firebaseApp";
 /**  this function takes in a url and a method
  and automatically attaches the token
  to the authorization header.*/
-const authorizedRequest = async (url, method) => {
+const authorizedRequest = async (url, method, body = {}) => {
+  const HTML_METHOD = method.toUpperCase();
   try {
     //get the token off of the current user
     const token = await firebaseApp.auth().currentUser.getIdToken();
 
-    const options = {
-      method: method.toUpperCase(), // *GET, POST, PUT, DELETE, etc.
+    let options = {
+      method: HTML_METHOD, // *GET, POST, PUT, DELETE, etc.
       headers: {
+        "Content-Type": "application/json",
         Authorization: token
       }
     };
+    if (HTML_METHOD !== "GET" || HTML_METHOD !== "HEAD") {
+      options.body = JSON.stringify(body);
+    }
+
     const responseFromServer = await fetch(url, options);
     console.log(responseFromServer);
     const json = await responseFromServer.json();
