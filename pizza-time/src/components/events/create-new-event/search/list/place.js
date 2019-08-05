443fb5620fff5809fea775fd31aef8f96d03a744
120356aa <yurlovandrew@gmail.com>
2019-08-05 15:15:43 -0700

    place code cleanup

--- pizza-time/src/components/events/create-new-event/search/list/place.js
+++ pizza-time/src/components/events/create-new-event/search/list/place.js
@@ -5,19 +5,14 @@ import { PlacesCard } from '../../../../../styles/placesSearchStyles';
 export default class Place extends Component {
     constructor(props) {
         super(props);
-        this.state = {
-            active: false
-        }
+        this.state = { active: false }
     }
 
     handleToggle = () => {
-        this.setState({
-            active: !this.state.active
-        })
+        this.setState({ active: !this.state.active })
     }
 
     handleClick = (e) => {
-        console.log('clicked')
         this.handleToggle();
         this.props.handleClick(this.props.data.place_id, this.props.data.name)
     }
@@ -27,12 +22,8 @@ export default class Place extends Component {
             <PlacesCard onClick={() => this.handleClick()} active={this.state.active}>
                 <div>
                     <p>{this.props.data.name}</p>
-                    <p className="rating">
-                        Rating: <Span rating={this.props.data.rating}>{this.props.data.rating}</Span>
-                    </p>
-                    <p>
-                        Location: <span>{this.props.data.formatted_address}</span>
-                    </p>
+                    <p className="rating">Rating: <Span rating={this.props.data.rating}>{this.props.data.rating}</Span></p>
+                    <p>Location: <span>{this.props.data.formatted_address}</span></p>
                 </div>
             </PlacesCard>
         )
