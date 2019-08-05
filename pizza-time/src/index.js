42cc7211736a01f2e58685d2aaf1c31ffbbe4b88
120356aa <yurlovandrew@gmail.com>
2019-08-05 15:00:46 -0700

    index code cleanup

--- pizza-time/src/index.js
+++ pizza-time/src/index.js
@@ -4,31 +4,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { BrowserRouter as Router } from "react-router-dom";
-// import * as serviceWorker from './serviceWorker';
-//redux dependencies
+
 import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import { Provider } from "react-redux";
 import logger from "redux-logger";
 import rootReducer from "./reducers";
 
-
-// ReactDOM.render(<App />, document.getElementById('root'));
-
-// // If you want your app to work offline and load faster, you can change
-// // unregister() to register() below. Note this comes with some pitfalls.
-// // Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
-
-
-// import * as serviceWorker from './serviceWorker';
-
-
-const store = createStore(
-  rootReducer, // this is the most basic reducer. A function that returns an object.
-  {}, //default state
-  applyMiddleware(thunk, logger) //middleware
-);
+const store = createStore(rootReducer, {}, applyMiddleware(thunk, logger));
 
 ReactDOM.render(
   <Provider store={store}>
@@ -38,6 +21,3 @@ ReactDOM.render(
   </Provider>,
   document.getElementById("root")
 );
-
-// // If you want your app to work offline and load faster, you can change
-// // unregister() to register() below. Note this comes with some pitfalls.
